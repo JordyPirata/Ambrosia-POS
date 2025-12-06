629697a708ecd70c42a7f40fd44aff55be4f3dcf
Ivan Robles <irae45@gmail.com>
2025-12-06 00:32:03 -0600

    fix currency select input.

--- client/src/components/pages/Onboarding/Onboarding.jsx
+++ client/src/components/pages/Onboarding/Onboarding.jsx
@@ -24,7 +24,7 @@ export function Onboarding() {
     businessPhone: "",
     businessEmail: "",
     businessRFC: "",
-    businessCurrency: "MXN",
+    businessCurrency: "USD",
     businessLogo: null,
   })
   const router = useRouter();
